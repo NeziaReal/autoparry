ba6f4ecfc199051aa2b2d4fbea900639eb28a334
NeziaReal <skibiditoilet123gyattnorizz@gmail.com>
2026-02-13 03:10:03 +0530

    Update ethossuite.lua

--- ethossuite.lua
+++ ethossuite.lua
@@ -1054,7 +1054,7 @@ function Library:New(options)
                     Parent = Btn,
                     Text = "Click",
                     Font = Enum.Font.GothamBold,
-                    TextColor3 = UI_CONFIG.Accent,
+                    TextColor3 = Color3.fromRGB(255, 255, 255),
                     TextSize = 11,
                     BackgroundTransparency = 1,
                     Size = UDim2.new(1, 0, 1, 0),
